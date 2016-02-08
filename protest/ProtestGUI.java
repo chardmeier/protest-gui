64897d851ebcb4008f742d845d00f66aef468b6a
lianEG <liane.guillou@hotmail.co.uk>
2016-02-08 17:05:53 +0100

    Amended browser window to dynamically list categories, with 'done', 'new' and 'conflicts' buttons for each, and SQL code to make the relevant selection

--- protest/ProtestGUI.java
+++ protest/ProtestGUI.java
@@ -21,53 +21,67 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.SwingUtilities;
 
-public class ProtestGUI implements Runnable, ListSelectionListener {
+//For message dialogs
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+public class ProtestGUI implements Runnable{
 	private Connection db_;
 
 	private ArrayList<String> categoryNames_;
-	private ArrayList<ArrayList<TestSuiteExample>> examplesByCategory_;
+	private ArrayList<ArrayList<TestSuiteExample>> conflictExamplesByCategory_;
     private ArrayList<ArrayList<TestSuiteExample>> newExamplesByCategory_;
     private ArrayList<ArrayList<TestSuiteExample>> doneExamplesByCategory_;
 
-	private JList categoryList_;
 	private InstanceWindow instWindow_;
+    private JFrame frame_;
+    
 
-	public ProtestGUI(String dbfile) throws SQLException {
-		db_ = DriverManager.getConnection("jdbc:sqlite:" + dbfile);
+    public ProtestGUI(String dbfile) throws SQLException {
+        db_ = DriverManager.getConnection("jdbc:sqlite:" + dbfile);
 
-		categoryNames_ = new ArrayList<String>();
-		examplesByCategory_ = new ArrayList<ArrayList<TestSuiteExample>>();
+        refreshData();
+
+        instWindow_ = new InstanceWindow();
+    }
+
+
+    public void refreshData() {
+        categoryNames_ = new ArrayList<String>();
+        conflictExamplesByCategory_ = new ArrayList<ArrayList<TestSuiteExample>>();
         newExamplesByCategory_ = new ArrayList<ArrayList<TestSuiteExample>>();
         doneExamplesByCategory_ = new ArrayList<ArrayList<TestSuiteExample>>();
-		Statement stmt = db_.createStatement();
-        ResultSet rs = stmt.executeQuery("select id as category_no, description from categories");
-        while(rs.next()) {
-            String name = rs.getString(2);
-			categoryNames_.add(name);
-            PreparedStatement ps = db_.prepareStatement("select example_no, srccorpus, tgtcorpus from pro_examples where category_no=?");
-            PreparedStatement done_ps = db_.prepareStatement("select p.example_no as example_no, p.srccorpus as srccorpus, p.tgtcorpus as tgtcorpus from pro_examples as p left join annotations as a where p.id=a.example and p.category_no=?");
-            PreparedStatement new_ps = db_.prepareStatement("select p.example_no as example_no, p.srccorpus as srccorpus, p.tgtcorpus as tgtcorpus from pro_examples as p where p.id not in (select distinct(example) from annotations) and p.category_no=?");
-            ps.setInt(1, rs.getInt("category_no"));
-            new_ps.setInt(1, rs.getInt("category_no"));
-            done_ps.setInt(1, rs.getInt("category_no"));
-			ResultSet exrs = ps.executeQuery();
-            ResultSet newExrs = new_ps.executeQuery();
-            ResultSet doneExrs = done_ps.executeQuery();
-            examplesByCategory_.add(addExamples(exrs));
-            newExamplesByCategory_.add(addExamples(newExrs));
-            doneExamplesByCategory_.add(addExamples(doneExrs));
-		}
+        try {
+            Statement stmt = db_.createStatement();
+            ResultSet rs = stmt.executeQuery("select id as category_no, description from categories");
+            while(rs.next()) {
+                String name = rs.getString(2);
+                categoryNames_.add(name);
+                String conflictCondition = "(select distinct(a.example) from annotations as a left join pro_examples as p on p.id=a.example left join token_annotations as ta on ta.example=a.example left join translations as t on p.example_no=t.example_no and p.tgtcorpus=t.tgtcorpus and ta.token=t.tgtpos and ta.line=t.line where (t.ant_no IS NOT NULL and a.ant_annotation IS NOT 'ok') or (a.ant_annotation IS 'ok' and ta.annotation IS NULL) UNION select distinct(a.example) from annotations as a left join pro_examples as p on p.id=a.example left join token_annotations as ta on ta.example=a.example left join translations as t on p.example_no=t.example_no and p.tgtcorpus=t.tgtcorpus and ta.token=t.tgtpos and ta.line=t.line where (t.ant_no IS NULL and a.anaph_annotation IS NOT 'ok' and NOT (a.anaph_annotation IN ('','bad') and ta.annotation IS NULL)) or (a.anaph_annotation IS 'ok' and ta.annotation IS NULL))";
+                PreparedStatement conflict_ps = db_.prepareStatement("select example_no, srccorpus, tgtcorpus from pro_examples where id in " + conflictCondition + " and category_no=?");
+                PreparedStatement done_ps = db_.prepareStatement("select p.example_no as example_no, p.srccorpus as srccorpus, p.tgtcorpus as tgtcorpus from pro_examples as p left join annotations as a where p.id=a.example and p.id not in " + conflictCondition + " and p.category_no=?");
+                PreparedStatement new_ps = db_.prepareStatement("select p.example_no as example_no, p.srccorpus as srccorpus, p.tgtcorpus as tgtcorpus from pro_examples as p where p.id not in (select distinct(example) from annotations) and p.category_no=?");
+                conflict_ps.setInt(1, rs.getInt("category_no"));
+                new_ps.setInt(1, rs.getInt("category_no"));
+                done_ps.setInt(1, rs.getInt("category_no"));
+                ResultSet conflictExrs = conflict_ps.executeQuery();
+                ResultSet newExrs = new_ps.executeQuery();
+                ResultSet doneExrs = done_ps.executeQuery();
+                conflictExamplesByCategory_.add(addExamples(conflictExrs));
+                newExamplesByCategory_.add(addExamples(newExrs));
+                doneExamplesByCategory_.add(addExamples(doneExrs));
+                }
+            }
+        catch(SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
-		instWindow_ = new InstanceWindow();
-	}
-    
+
     public ArrayList<TestSuiteExample> addExamples(ResultSet rs) throws SQLException {
         ArrayList<TestSuiteExample> result = new ArrayList<TestSuiteExample>();
         while(rs.next()) {
@@ -77,19 +91,33 @@ public class ProtestGUI implements Runnable, ListSelectionListener {
         }
         return result;
     }
+   
+    
+    public void onButtonClick(List<TestSuiteExample> listInstances, String c, String msg) {
+        if (listInstances.isEmpty()){
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            JOptionPane.showMessageDialog(frame_, msg);
+        }
+        else {
+            instWindow_.setData(c, listInstances);
+            instWindow_.setVisible(true);
+        }
+    }
 
+    
 	public void run() {
-		JFrame frame = new JFrame("PROTEST Browser");
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		((BorderLayout) frame.getContentPane().getLayout()).setVgap(15);
-
-		categoryList_ = new JList(categoryNames_.toArray(new String[0]));
-		categoryList_.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		categoryList_.addListSelectionListener(this);
-		frame.getContentPane().add(new JScrollPane(categoryList_), BorderLayout.PAGE_START);
+		frame_ = new JFrame("PROTEST Browser");
+		frame_.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		((BorderLayout) frame_.getContentPane().getLayout()).setVgap(15);
+        
+        JLabel instructionLabel_ = new JLabel("<html><b>Select set of instances, for annotation / browsing:</b></html>");
+        JPanel instructionPanel_ = new JPanel();
+        instructionPanel_.setLayout(new FlowLayout(FlowLayout.LEFT));
+        instructionPanel_.add(instructionLabel_, BorderLayout.LINE_START);
+        frame_.getContentPane().add(instructionPanel_, BorderLayout.PAGE_START);
         
         JPanel catButtonsPanel_ = new JPanel(new GridLayout(14,1));
-        frame.getContentPane().add(new JScrollPane(catButtonsPanel_), BorderLayout.LINE_START);
+        frame_.getContentPane().add(new JScrollPane(catButtonsPanel_), BorderLayout.LINE_START);
         
         for(String c : categoryNames_) {
             JPanel rowPanel_ = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -105,18 +133,21 @@ public class ProtestGUI implements Runnable, ListSelectionListener {
             conflictsButton.setOpaque(true);
             newButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    instWindow_.setData(c, newExamplesByCategory_.get(categoryNames_.indexOf(c)));
-                    instWindow_.setVisible(true);
+                    refreshData();
+                    List<TestSuiteExample> listInstances = newExamplesByCategory_.get(categoryNames_.indexOf(c));
+                    onButtonClick(listInstances, c, "No unannotated examples for this category");
                 }});
             doneButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    instWindow_.setData(c, doneExamplesByCategory_.get(categoryNames_.indexOf(c)));
-                    instWindow_.setVisible(true);
+                    refreshData();
+                    List<TestSuiteExample> listInstances = doneExamplesByCategory_.get(categoryNames_.indexOf(c));
+                    onButtonClick(listInstances, c, "No annotated examples for this category");
                 }});
             conflictsButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    instWindow_.setData(c, examplesByCategory_.get(categoryNames_.indexOf(c)));
-                    instWindow_.setVisible(true);
+                    refreshData();
+                    List<TestSuiteExample> listInstances = conflictExamplesByCategory_.get(categoryNames_.indexOf(c));
+                    onButtonClick(listInstances, c, "No examples with conflicting annotations for this category");
                 }});
             rowPanel_.add(catLabel);
             rowPanel_.add(newButton);
@@ -126,24 +157,17 @@ public class ProtestGUI implements Runnable, ListSelectionListener {
         }
 
 		JButton quitButton = new JButton("Quit");
-		frame.getContentPane().add(quitButton, BorderLayout.PAGE_END);
+		frame_.getContentPane().add(quitButton, BorderLayout.PAGE_END);
 		quitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
 
-		frame.pack();
-		frame.setVisible(true);
+		frame_.pack();
+		frame_.setVisible(true);
 	}
 
-	public void valueChanged(ListSelectionEvent e) {
-		if(e.getValueIsAdjusting())
-			return;
-		int idx = categoryList_.getMinSelectionIndex();
-		instWindow_.setData(categoryNames_.get(idx), examplesByCategory_.get(idx));
-		instWindow_.setVisible(true);
-	}
 
 	public static void main(String[] args) throws SQLException {
 		if(args.length != 1) {
