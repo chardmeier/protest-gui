79aa87d308b249ce0ffa350ca3b9c226b5eb63b8
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2016-02-11 15:09:24 +0100

    Added task_no field to tasks table.

--- AddToDatabase.sql
+++ AddToDatabase.sql
@@ -14,7 +14,7 @@ DROP TABLE IF EXISTS meta_data;
 CREATE TABLE meta_data (
 	id integer primary key autoincrement,
 	tag text not null,
-    tag_value text not null
+	tag_value text not null
 );
 
 INSERT INTO meta_data (tag,tag_value) VALUES ("file_type","MasterDB");
@@ -23,8 +23,8 @@ INSERT INTO meta_data (tag,tag_value) VALUES ("master_id","DiscoMT2015");
 DROP TABLE IF EXISTS annotation_tasks;
 CREATE TABLE annotation_tasks (
 	id integer primary key autoincrement,
-    example integer,
-	annotator_id int
+	task_no integer not null,
+	annotator_id integer not null,
+	example integer not null
 );
-
 
