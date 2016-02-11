aa0b2c609aa6f9f89a04c61727bbeaf7eed83e74
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2016-02-11 10:30:34 +0100

    Removed unused imports.

--- protest/ProtestGUI.java
+++ protest/ProtestGUI.java
@@ -5,14 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
-import java.awt.Color;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import java.util.ArrayList;
 import java.util.List;
