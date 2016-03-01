5d5385b3dd06cbb66b72f163d8c5acb0c90e7acb
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2016-03-01 15:59:09 +0100

    Add id field to tag_annotations table.

--- update-to-v1.1.sql
+++ update-to-v1.1.sql
@@ -1,8 +1,7 @@
 update meta_data set tag_value='PROTEST 1.1' where tag='file_version';
 
 create table tag_annotations (
+	id int not null primary key,
 	candidate int not null,
 	annotator_id int not null,
-	tag text not null,
-	primary key (candidate, annotator_id, tag));
-
+	tag text not null);
