1ddd6a7a6c544954b69114ab822b4c0efd2a504c
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2016-03-18 10:03:46 +0100

    Removed unused imports.

--- protest/db/TestSuiteExample.java
+++ protest/db/TestSuiteExample.java
@@ -9,8 +9,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class TestSuiteExample {
 	private Database db_;
