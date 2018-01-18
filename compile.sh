6a63403aaaffa317b481e20193b01f11ae3f8bcc
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2018-01-18 16:52:56 +0100

    Disabled lint:unchecked warnings.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 find protest -name \*.java |
-	xargs javac -Xlint:unchecked -Xlint:deprecation -classpath lib/flyingsaucer/core-renderer.jar:lib/sqlite-jdbc-3.8.11.2.jar:lib/jdbcdslog-1.0.5.jar:lib/slf4j-api-1.7.17.jar:lib/c3p0-0.9.5.2.jar:lib/mchange-commons-java-0.2.11.jar:.
+	xargs javac -Xlint:deprecation -classpath lib/flyingsaucer/core-renderer.jar:lib/sqlite-jdbc-3.8.11.2.jar:lib/jdbcdslog-1.0.5.jar:lib/slf4j-api-1.7.17.jar:lib/c3p0-0.9.5.2.jar:lib/mchange-commons-java-0.2.11.jar:.
